e88154becb2e3fa1b66b8bcc7d1cb6877d5e9ad6
Louis-Marie Matthews <internet657-github@yahoo.fr>
2017-02-19 13:14:31 +0000

    Add quiz creation page

---
+++ QuickQuiz/src/java/quickquiz/stores/Quiz.java
@@ -0,0 +1,32 @@
+/*
+ * QuickQuiz is a database application allowing staff to manage
+ * students to complete them.
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package quickquiz.stores;
+
+/**
+ *
+ * @author Louis-Marie Matthews
+ */
+public class Quiz
+{
+    private String name_;
+    private String moduleId_;
+    
+    
+    
+    public Quiz(String name, Integer moduleId)
+}

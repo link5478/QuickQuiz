85eaf2c9a2337f69ce72060be715744ef55935a8
Tene21 <craig_chicken@yahoo.co.uk>
2017-02-19 16:02:38 +0000

    removed an additional semicolon and added a null value to QuizCreation so netbeans would stop throwing errors

--- QuickQuiz/src/java/quickquiz/model/QuizModel.java
+++ QuickQuiz/src/java/quickquiz/model/QuizModel.java
@@ -19,6 +19,7 @@ package quickquiz.model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import quickquiz.lib.Database;

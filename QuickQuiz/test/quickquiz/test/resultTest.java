ea8b517b634386142ffd2f41ff0786f2a28f05a6
Broxibear97 <hogarthjoshie@aol.com>
2017-03-15 13:29:33 +0000

    glory greatest test.

--- QuickQuiz/test/quickquiz/test/resultTest.java
+++ QuickQuiz/test/quickquiz/test/resultTest.java
@@ -5,37 +5,59 @@
  */
 package quickquiz.test;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import org.junit.Test;
 import quickquiz.exception.UnrelatedAnswerException;
+import quickquiz.model.ResultsModel;
 
 import quickquiz.stores.Result;
+
 /**
  *
  * @author hogar
  */
 public class resultTest {
-    
-    
+
     @Test
-    public void ResultCheck()
-    {
+    public void ResultCheck() {
         Result r = new Result();
         r.addAnswer("5");
         List<String> expected = new ArrayList<>();
         expected.add("5");
         List<String> actual = r.getAnswers();
-        
-        try 
-        {
-                assertEquals(expected, actual);
-        }
-        catch (Exception e) 
-        {
+
+        try {
+            assertEquals(expected, actual);
+        } catch (Exception e) {
             fail("Expected didnt match actual. Issue storing answers?");
         }
+    }
+
+    @Test
+    public void getResultTest() {
+        int id = 3;
+        Result r;
+        
+        try
+        {
+            r = ResultsModel.getResult(id);
+        }
+        catch( ClassNotFoundException | IllegalAccessException | InstantiationException | SQLException e)
+        {
+            r = new Result();
+        }
+
+        Result expected = new Result();
+        expected.setMark(70.00f);
+        expected.setQuizId(2);
+        expected.setUserID("140023542");
+
+        assertEquals("mark should be the same", expected.getMark(), r.getMark(), 2);
+        assertEquals("quizID should be the same", expected.getQuizId(), r.getQuizId());
+        assertEquals("UseriD should be the same", expected.getUserID(), r.getUserID());
     }
 }

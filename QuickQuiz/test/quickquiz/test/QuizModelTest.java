08ac9c97f300b06da626aa8e0904aa1a8b6e76cc
Louis-Marie Matthews <internet657-github@yahoo.fr>
2017-03-16 02:43:49 +0000

    Update Quiz store class test case

--- QuickQuiz/test/quickquiz/test/QuizModelTest.java
+++ QuickQuiz/test/quickquiz/test/QuizModelTest.java
@@ -35,31 +35,6 @@ import quickquiz.stores.Quiz;
  */
 public class QuizModelTest
 {
-  @Test
-  public void testQuiz()
-  {
-    Question question1 = new Question("How old is Virgil?", "18", "20", "21",
-                                      "22", "Because he was born on July " +
-                                      "12th 1994", 1, 1, 1);
-    Question question2 = new Question("How old is Louis?", "17", "18", "19",
-                                      "20", "Because he was born on February " +
-                                      "22nd 1997", 2, 1, 1);
-    assertEquals("The question doesn't return correctly.",
-                 question1.getQuestionText(), "How old is Virgil?");
-    Quiz quiz1 = new Quiz("Guess someone's age", "You are challenged to " +
-                          "guess someone's age by guessing.", "AC690069",
-                          "Age guessing education", "Louis-Marie Matthews");
-    assertEquals("The quiz doesn't return correctly.", quiz1.getName(),
-                 "Guess someone's age");
-    quiz1.getQuestions().add(question1);
-    assertEquals("The original question and the retrieved question are not " +
-                 "the same", quiz1.getQuestions().get(0), question1);
-    assertEquals("The size is not the same as the one tested",
-                 quiz1.getNumberOfQuestions(), 1);
-  }
-  
-  
-  
   /**
    * If this test fails, it might be because quizzes have been updated in the
    * database. The number at the very beginning of the method just need to be

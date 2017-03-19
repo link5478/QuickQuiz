b76af427373ba395b2613f4567873b4adf7cee80
Louis-Marie Matthews <internet657-github@yahoo.fr>
2017-03-19 17:19:32 +0000

    Add link to edit quiz in view-quiz

--- QuickQuiz/src/java/quickquiz/model/ModuleModel.java
+++ QuickQuiz/src/java/quickquiz/model/ModuleModel.java
@@ -31,6 +31,14 @@ import quickquiz.stores.Module;
  */
 public class ModuleModel
 {
+  /**
+   * TODO: replace set by list
+   * @return
+   * @throws SQLException
+   * @throws ClassNotFoundException
+   * @throws InstantiationException
+   * @throws IllegalAccessException 
+   */
   public static List<Module> getModules()
     throws SQLException, ClassNotFoundException, InstantiationException,
            IllegalAccessException
